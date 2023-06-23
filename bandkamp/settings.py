faef861aaccbe895fd25770de6883a054a8ab4d1
AdrianoAraujo <araujoadriano95@gmail.com>
2023-06-23 13:53:09 -0300

    tarefa 1 feita

--- bandkamp/settings.py
+++ bandkamp/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
+import dotenv
+
+dotenv.load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-e91823y78dgasdas89cvyxz76the912j321321-cx9!!#1232111!!!"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -89,7 +93,17 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
-    }
+    },
+
+    "default": {
+      # O django já contém a instrução para rodar o motor psycopg2 do postgres
+      "ENGINE": "django.db.backends.postgresql",
+      "NAME": os.getenv("DB_NAME"),
+      "USER": os.getenv("DB_USER"),
+      "PASSWORD": os.getenv("DB_PASSWORD"),
+      "HOST": "127.0.0.1",
+      "PORT": 5432,
+  }
 }
 
 
